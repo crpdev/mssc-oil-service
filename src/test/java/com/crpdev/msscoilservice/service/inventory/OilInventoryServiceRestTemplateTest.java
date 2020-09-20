ea81cfa4b6e8ac7535d264d1f6aec03257730899
Rajapandian C <rajapandianc@outlook.in>
2020-09-20 06:55:28 +0530

    Dockerized Oil Service using Fabric8 Maven Plugin

--- src/test/java/com/crpdev/msscoilservice/service/inventory/OilInventoryServiceRestTemplateTest.java
+++ src/test/java/com/crpdev/msscoilservice/service/inventory/OilInventoryServiceRestTemplateTest.java
@@ -3,20 +3,26 @@ package com.crpdev.msscoilservice.service.inventory;
 import com.crpdev.msscoilservice.bootstrap.OilLoader;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Profile;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
 
 @Disabled
 @Profile("!local-discovery")
 @SpringBootTest
 class OilInventoryServiceRestTemplateTest {
 
-    @Autowired
+    @MockBean
     OilInventoryService oilInventoryService;
 
     @Test
+    @Disabled
     void getOnHandInventory(){
+        given(oilInventoryService.getOnHandInventory(any())).willReturn(anyInt());
         Integer onHand = oilInventoryService.getOnHandInventory(OilLoader.OIL_1_UUID);
         System.out.println(onHand);
     }
